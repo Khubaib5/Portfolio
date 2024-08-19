0311abbccb34bbcbe364a4d96aeb9fbf6579885f
Khubaib <khubaibwasim75@gmail.com>
2024-08-19 22:26:33 +0500

    last one

--- app/head.jsx
+++ app/head.jsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 // components/BotpressWebchat.js
-import {useEffect}  from 'react';
+import { useEffect } from 'react';
 
 const BotpressWebchat = () => {
   useEffect(() => {
     // Dynamically load the Botpress scripts
     const injectScript = document.createElement('script');
-    injectScript.src = 'https://cdn.botpress.cloud/webchat/v2/inject.js';
+    injectScript.src = 'https://cdn.botpress.cloud/webchat/v2.1/inject.js';
     injectScript.async = true;
     document.body.appendChild(injectScript);
 
     const configScript = document.createElement('script');
-    configScript.src = 'https://mediafiles.botpress.cloud/270d7db1-3e2f-4f71-aefe-61f945259f91/webchat/v2/config.js';
+    configScript.src = 'https://mediafiles.botpress.cloud/e9ae0766-531f-404c-b3d7-5ce841aeb19c/webchat/v2.1/config.js';
     configScript.defer = true;
     document.body.appendChild(configScript);
 
